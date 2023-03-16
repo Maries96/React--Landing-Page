5b86af812038aaa4991079208ad0691a6345f9a1
Marie <marievstewart@gmail.com>
2023-03-16 00:44:02 +0000

    React Landing Page

---
+++ src/pages/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export function Footer () {
+    return <footer class="text-center text-white fixed-bottom" style={{Color:"grey"}}>
+   
+    <div class="container p-4"></div>
+  
+    <div class="text-center p-3" style={{backgroundColor: "rgba(0, 0, 0, 0.2)"}}>
+      © 2020 Copyright:
+      <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    </div>
+    
+  </footer>
+}
---
+++ src/pages/components/Jumbotron.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export function Jumbotron () {
+    return <div className="jumbotron">
+  <h1 className="display-4">I am Learning React!</h1>
+  <p className="lead">React is showing to be user-friendly, except I can't get the navbar to move the stuff to the right...</p>
+  <hr className="my-4"></hr>
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <p className="lead">
+    <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  </p>
+</div>
+};
